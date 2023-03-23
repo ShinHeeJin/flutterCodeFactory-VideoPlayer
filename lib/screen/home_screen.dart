6d182855600f1141d05d7292191fe995c42bdc72
ShinHeeJin <sisesu92@gmail.com>
2023-03-24 01:24:12 +0900

    feat : EmptyScreen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("HomeScreen"),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: video == null ? renderEmpty() : renderVideo(),
+    );
+  }
+
+  Widget renderEmpty() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          _Logo(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _AppName(),
+        ],
+      ),
+    );
+  }
+
+  Widget renderVideo() {
+    return Container();
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("asset/img/logo.png");
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName();
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "VIDEO",
+          style: textStyle,
+        ),
+        Text(
+          "PLAYER",
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
     );
   }
 }
