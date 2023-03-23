6decddf397476cd5ecba46e41fdbdf55c75a6849
ShinHeeJin <sisesu92@gmail.com>
2023-03-24 01:46:29 +0900

    feat : VideoPlayerController

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
@@ -11,14 +14,40 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeController();
+  }
+
+  initializeController() async {
+    final videoPlayerController = VideoPlayerController.file(
+      File(
+        widget.video.path,
+      ),
+    );
+
+    await videoPlayerController.initialize();
+
+    setState(() {
+      this.videoPlayerController = videoPlayerController;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "CustomVideoPlayer",
-        style: TextStyle(
-          color: Colors.white,
-        ),
+    if (videoPlayerController == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: videoPlayerController!.value.aspectRatio,
+      child: VideoPlayer(
+        videoPlayerController!,
       ),
     );
   }
