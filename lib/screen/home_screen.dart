1486bc0129a47c3ec5429f10e30ab40ce1aa2a26
ShinHeeJin <sisesu92@gmail.com>
2023-03-24 01:38:06 +0900

    feat : set CustomVideoPlayer Base

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:vid_player/component/custom_video_player.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -62,7 +63,11 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget renderVideo() {
-    return Container();
+    return Center(
+      child: CustomVideoPlayer(
+        video: video!,
+      ),
+    );
   }
 }
 
