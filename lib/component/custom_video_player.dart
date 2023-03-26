24a3505dcf04b6b9508eedf223e7d4ecd44e9923
ShinHeeJin <sisesu92@gmail.com>
2023-03-27 02:28:06 +0900

    feat : hide contoller

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -18,6 +18,7 @@ class CustomVideoPlayer extends StatefulWidget {
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   VideoPlayerController? videoPlayerController;
+  bool showControls = false;
 
   @override
   void initState() {
@@ -58,60 +59,74 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
       );
     }
 
-    return AspectRatio(
-      aspectRatio: videoPlayerController!.value.aspectRatio,
-      child: Stack(
-        children: [
-          VideoPlayer(
-            videoPlayerController!,
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Slider(
-              onChanged: (double val) {
-                videoPlayerController!.seekTo(
-                  Duration(
-                    seconds: val.toInt(),
-                  ),
-                );
-              },
-              value: videoPlayerController!.value.position.inSeconds.toDouble(),
-              min: 0,
-              max: videoPlayerController!.value.duration.inSeconds.toDouble(),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          showControls = !showControls;
+        });
+      },
+      child: AspectRatio(
+        aspectRatio: videoPlayerController!.value.aspectRatio,
+        child: Stack(
+          children: [
+            VideoPlayer(
+              videoPlayerController!,
             ),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: CustomIconButton(
-              onPressed: widget.onNewVideoPressed,
-              iconData: Icons.photo_camera_back,
+            if (showControls)
+              Container(
+                color: Colors.black.withOpacity(0.5),
+              ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Slider(
+                onChanged: (double val) {
+                  videoPlayerController!.seekTo(
+                    Duration(
+                      seconds: val.toInt(),
+                    ),
+                  );
+                },
+                value:
+                    videoPlayerController!.value.position.inSeconds.toDouble(),
+                min: 0,
+                max: videoPlayerController!.value.duration.inSeconds.toDouble(),
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CustomIconButton(
-                  onPressed: onReversePressed,
-                  iconData: Icons.rotate_left,
+            if (showControls)
+              Align(
+                alignment: Alignment.topRight,
+                child: CustomIconButton(
+                  onPressed: widget.onNewVideoPressed,
+                  iconData: Icons.photo_camera_back,
                 ),
-                CustomIconButton(
-                  onPressed: onPlayPressed,
-                  iconData: videoPlayerController!.value.isPlaying
-                      ? Icons.pause
-                      : Icons.play_arrow,
+              ),
+            if (showControls)
+              Align(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CustomIconButton(
+                      onPressed: onReversePressed,
+                      iconData: Icons.rotate_left,
+                    ),
+                    CustomIconButton(
+                      onPressed: onPlayPressed,
+                      iconData: videoPlayerController!.value.isPlaying
+                          ? Icons.pause
+                          : Icons.play_arrow,
+                    ),
+                    CustomIconButton(
+                      onPressed: onForwardPressed,
+                      iconData: Icons.rotate_right,
+                    ),
+                  ],
                 ),
-                CustomIconButton(
-                  onPressed: onForwardPressed,
-                  iconData: Icons.rotate_right,
-                ),
-              ],
-            ),
-          )
-        ],
+              )
+          ],
+        ),
       ),
     );
   }
